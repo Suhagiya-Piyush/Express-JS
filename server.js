bf88adfca4e724472084b09ffa961429fa4bcf9f
Suhagiya Piyush <piyush.suhagiya.ps@gmail.com>
2024-08-10 09:38:08 +0530

    Day-3

--- server.js
+++ server.js
@@ -1,55 +1,48 @@
-// nom init / npm init -y
+/*// todo -------------------------------- Lesson 3 (07/08/24)-------------------------------- */
 
-const express = require("express");
-
-// http.createSeaver();
+const express = require('express');
 const server = express();
+const data = require('./Data/friends.json');
+// console.log(data);
 
-// CRUD -> Create(POST), Read(GET), Update(PUT, PATCH), Delete(DELETE)
-server.get('/', (req, res) => {
-    res.write('Welcome to Express Server');
-    res.end();
+const morgan = require('morgan');
+
+// 4.0 verson -> body-parser
+// express.json -> raw / json formate
+// express.urlencoded -> form
+// express.static
+
+server.use(express.json());
+// server.use(express.urlencoded({extended:true}));
+// server.use('/hello', express.static('./Data/friends.json'));
+server.use(morgan('dev'));
+
+let middelware = (req, res, next) => {
+    // console.log(req.query);
+    if(req.query.age >= 18){
+        // console.log('Success');
+        next();
+    }else{
+        return res.json({message : 'Inccorect Way!!!'});
+    }
+}
+let loggerFun = (req, res, next) => {
+    console.log(req.url, "\t", req.method, "\t");
+    next();
+}
+server.use(loggerFun)
+
+/* ---------------------------- Application Level --------------------------- */
+// server.use(middelware);
+
+server.get('/',middelware, (req, res) => {
+    res.send({message : 'Welcome to Express Server'});
 });
-// server.get('/login', (req, res) => {
-//     res.send('Welcome to Login Page');
-// });
 
-server.get("/user", (req, res) => {
-    res.status(200);
-    res.json({message : 'User GET Method'});
-})
-server.post("/user", (req, res) => {
-    res.status(201);
-    res.json({message : 'User POST Method'});
-})
-server.put("/user", (req, res) => {
-    res.status(202);
-    res.json({message : 'User PUT Method'});
-})
-server.patch("/user", (req, res) => {
-    // res.status(199);
-    res.json({message : 'User PATCH Method'});
-})
-server.delete("/user", (req, res) => {
-    res.status(400);
-    res.json({message : 'User DELETE Method'});
-})
-server.patch("/admin", (req, res) => {
-    res.status(404);
-    res.json({message : 'Admin PATCH Method'});
-})
-server.delete("/admin", (req, res) => {
-    res.status(500);
-    res.json({message : 'Admin DELETE Method'});
-})
+server.get('/friend', (req, res) => {
+    res.json(data);
+});
 
 server.listen(1234, () => {
-    console.log(`Server Start at http://localhost:1234`);
+    console.log('Server start at http://localhost:1234');
 });
-
-
-
-// git checkout -b branchName
-// git add . 
-// git commit -m "your commit"
-// git push -u origin branchName
